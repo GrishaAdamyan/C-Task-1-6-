187a7eb75f508c66f4265fa970a34a5708eff78b
GrishaAdamyan <72817828+GrishaAdamyan@users.noreply.github.com>
2024-01-31 00:46:59 +0400

    Add files via upload

---
+++ Task1(Knight).cs
@@ -0,0 +1,79 @@
+﻿//1 version
+
+/*Console.WriteLine("enter the number of row:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the number of column:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+if (((9 > (a - 2)) && ((a - 2) > 0) && (9 > (b - 1)) && ((b - 1) > 0)))
+{
+    Console.WriteLine("the number of row is " + (a - 2));
+    Console.WriteLine("the number of column is " + (b - 1));
+}
+if (((9 > (a - 2)) && ((a - 2) > 0) && (9 > (b + 1)) && ((b + 1) > 0)))
+{
+    Console.WriteLine("the number of row is " + (a - 2));
+    Console.WriteLine("the number of column is " + (b + 1));
+}
+if (((9 > (a - 1)) && ((a - 1) > 0) && (9 > (b - 2)) && ((b - 2) > 0)))
+{
+    Console.WriteLine("the number of row is " + (a - 1));
+    Console.WriteLine("the number of column is " + (b - 2));
+}
+if (((9 > (a - 1)) && ((a - 1) > 0) && (9 > (b + 2)) && ((b + 2) > 0)))
+{
+    Console.WriteLine("the number of row is " + (a - 1));
+    Console.WriteLine("the number of column is " + (b + 2));
+}
+if (((9 > (a + 1)) && ((a + 1) > 0) && (9 > (b - 2)) && ((b - 2) > 0)))
+{
+    Console.WriteLine("the number of row is " + (a + 1));
+    Console.WriteLine("the number of column is " + (b - 2));
+}
+if (((9 > (a + 1)) && ((a + 1) > 0) && (9 > (b + 2)) && ((b + 2) > 0)))
+{
+    Console.WriteLine("the number of row is " + (a + 1));
+    Console.WriteLine("the number of column is " + (b + 2));
+}
+if (((9 > (a + 2)) && ((a + 2) > 0) && (9 > (b - 1)) && ((b - 1) > 0)))
+{
+    Console.WriteLine("the number of row is " + (a + 2));
+    Console.WriteLine("the number of column is " + (b - 1));
+}
+if (((9 > (a + 2)) && ((a + 2) > 0) && (9 > (b + 1)) && ((b + 1) > 0)))
+{
+    Console.WriteLine("the number of row is " + (a + 2));
+    Console.WriteLine("the number of column is " + (b + 1));
+}*/
+
+
+
+
+//2 version
+
+/*Console.WriteLine("enter the number of row:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the number of column:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 1; i < 9; i++)
+{
+    for (int j = 1; j < 9; j++)
+    {
+        if ((Math.Abs(a - i) == 2 && Math.Abs(b - j) == 1) || (Math.Abs(a - i) == 1 && Math.Abs(b - j) == 2))
+        {
+            Console.Write("1 ");
+        }
+        else if (a == i && b == j)
+        {
+            Console.Write("K ");
+        }
+        else
+        {
+            Console.Write("0 ");
+        }
+    }
+    Console.WriteLine();
+}*/
---
+++ Task3.cs
@@ -0,0 +1,68 @@
+﻿/*Console.WriteLine("enter the number of row:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the number of column:");
+int b = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[8, 8];
+int flag = 0;
+
+do{
+    int count = addingQueen(a, b);
+    if (count == 2)
+    {
+        flag = 1;
+    }
+    Random rnd = new Random();
+    int index = rnd.Next(2, count + 1);
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (matrix[i, j] == index)
+            {
+                a = i;
+                b = j;
+                break;
+            }
+        }
+    }
+}
+while (flag == 0);
+
+Console.WriteLine();
+for (int i = 0; i < 8; i++)
+{
+    for (int j = 0; j < 8; j++)
+    {
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+int addingQueen(int a, int b)
+{
+    int count = 2;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (a == i && b == j)
+            {
+                matrix[i, j] = 777;
+            }
+            else if (matrix[i, j] == 1 || matrix[i, j] == 777)
+            {
+                continue;
+            }
+            else if ((Math.Abs(a - i) == Math.Abs(b - j)) || a == i || b == j)
+            {
+                matrix[i, j] = 1;
+            }
+            else
+            {
+                matrix[i, j] = count;
+                count++;
+            }
+        }
+    }
+    return count;
+}*/
