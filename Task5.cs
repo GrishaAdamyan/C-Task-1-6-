ad970949389878dc9c6fcda4db6a0925e91f5089
GrishaAdamyan <72817828+GrishaAdamyan@users.noreply.github.com>
2024-02-02 12:10:06 +0400

    Add files via upload

--- Task5.cs
+++ Task5.cs
@@ -1,4 +1,4 @@
-﻿int[,] matrix = { { 1, 2, 3, 4, 4 }, { 4, 5, 6, 10, 5 } };
+﻿int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 int a = matrix.GetLength(0);
 int b = matrix.GetLength(1);
 int count = 0;
